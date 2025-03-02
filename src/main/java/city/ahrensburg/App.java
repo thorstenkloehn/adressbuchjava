c35a2b8010530763ff804b5f3cfe82cc1c823fc7
Thorsten Klöhn <thorstenkloehn@gmail.com>
2025-03-02 14:33:29 +0100

    Füge Benutzerinteraktion zur Hauptmethode hinzu

--- src/main/java/city/ahrensburg/App.java
+++ src/main/java/city/ahrensburg/App.java
@@ -1,5 +1,7 @@
 package city.ahrensburg;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        int wahl = 0;
+        do {
+            System.out.println("Bitte w#hlen Sie eine Option");
+            
+
+            wahl = scanner.nextInt();
+
+
+        } while (wahl != 0);
+        scanner.close();
     }
 }
