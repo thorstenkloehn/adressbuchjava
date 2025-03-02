cc59598b8b869d5f2f28b8a8ffff78d1fab390f2
Thorsten Klöhn <thorstenkloehn@gmail.com>
2025-03-02 11:54:03 +0100

    Füge Getter- und Setter-Methoden zur Klasse Kontakt hinzu

--- src/main/java/city/ahrensburg/Kontakt.java
+++ src/main/java/city/ahrensburg/Kontakt.java
@@ -14,4 +14,36 @@ public  Kontakt(String vorname,String nachname,String telefonnummer,String adres
 
 }
 
+public String getVorname() {
+    return vorname;
+}
+
+public void setVorname(String vorname) {
+    this.vorname = vorname;
+}
+
+public String getNachname() {
+    return nachname;
+}
+
+public void setNachname(String nachname) {
+    this.nachname = nachname;
+}
+
+public String getTelefonnummer() {
+    return telefonnummer;
+}
+
+public void setTelefonnummer(String telefonnummer) {
+    this.telefonnummer = telefonnummer;
+}
+
+public String getAdresse() {
+    return adresse;
+}
+
+public void setAdresse(String adresse) {
+    this.adresse = adresse;
+}
+
 }
