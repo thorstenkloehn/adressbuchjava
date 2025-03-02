008085a98ff3e412187350951a8e5fe6340d0d0d
Thorsten Klöhn <thorstenkloehn@gmail.com>
2025-03-02 03:00:21 +0100

    Füge Konstruktor zur Klasse Kontakt hinzu

--- src/main/java/city/ahrensburg/Kontakt.java
+++ src/main/java/city/ahrensburg/Kontakt.java
@@ -6,5 +6,12 @@ private String nachname;
 private String telefonnummer;
 private String adresse;
 
+public  Kontakt(String vorname,String nachname,String telefonnummer,String adresse) {
+    this.vorname = vorname;
+    this.nachname = nachname;
+    this.telefonnummer = telefonnummer;
+    this.adresse = adresse;
+
+}
 
 }
